077d854e060d0ab5a030459edf93eb049ebb4607
ductt7 <104555793+ductt7@users.noreply.github.com>
2024-04-06 11:28:18 +0700

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "ap-southeast-1"
+}
+
+
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
+
+
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+
+
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
+}
+
+resource "aws_instance" "ductt-ec2" {
+  count         = 5
+  ami           = data.amazon-linux-2.id
+  instance_type = "t2.micro"
+}
